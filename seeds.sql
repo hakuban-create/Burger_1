ece13d53fb1b4d694d8019c340d38bd366785587
Khaliunaa Baasandorj <khaliunaabaasandorj@Khaliunaas-MacBook-Pro-2.local>
2020-02-23 00:48:51 -0500

    added seeds

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, deleted) VALUE ('Double Quarter Pounder with Cheese', null);
+INSERT INTO burgers (name, deleted) VALUE ('Bacon Clubhouse Burger', null);
+INSERT INTO burgers (name, deleted) VALUE ('Cheeseburger', null);
