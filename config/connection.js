4234168734163ff70612f4f15c4428279fde3e65
Khaliunaa Baasandorj <khaliunaabaasandorj@Khaliunaas-MacBook-Pro-2.local>
2020-02-23 09:56:18 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql2");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "testtest",
     database: "burger1"
   });
+}
   
   connection.connect(function(err) {
     if (err) {
